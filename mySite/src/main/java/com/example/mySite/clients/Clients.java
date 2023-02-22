d76c470e520d981aa50916afa7e93f36f157df08
nazar <nhavronskyi@gmail.com>
2023-02-22 07:56:15 +0100

    create clients.java

---
+++ mySite/src/main/java/com/example/mySite/clients/Clients.java
@@ -0,0 +1,48 @@
+package com.example.mySite.clients;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Clients {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "pass", nullable = false)
+	private String pass;
+	
+	public Clients(String name, String pass) {
+		this.name = name;
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	@Override
+	public String toString() {
+		return "Clients [id=" + id + ", name=" + name + ", pass=" + pass + "]";
+	}
+}
