09488768935e2b4d180c829e2153d4a439ed2434
nazar <nhavronskyi@gmail.com>
2023-02-22 19:19:46 +0100

    delete user with id from line

--- mySite/src/main/java/com/example/mySite/clients/ClientController.java
+++ mySite/src/main/java/com/example/mySite/clients/ClientController.java
@@ -3,9 +3,9 @@ package com.example.mySite.clients;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +36,11 @@ public class ClientController {
 			@RequestParam(name = "name")String name, 
 			@RequestParam(name = "pass")String pass) {
 		clientService.addClient(new Client(name, pass));
+	}
+	
+	
+	@DeleteMapping("/deleteClient")
+	public void deleteClient(@RequestParam(name = "id") Long id) {
+		clientService.deleteClient(id);
 	}
 }
--- mySite/src/main/java/com/example/mySite/clients/ClientService.java
+++ mySite/src/main/java/com/example/mySite/clients/ClientService.java
@@ -21,4 +21,12 @@ public class ClientService {
 	public void addClient(Client client) {
 		clientsRepository.save(client);
 	}
+
+	public void deleteClient(Long id) {
+		boolean exist = clientsRepository.existsById(id);
+		if(!exist)
+			throw new IllegalStateException("student with id: " + id + " does not exist");
+		else 
+			clientsRepository.deleteById(id);
+	}
 }
