5205f870d2456b566acde763251535d506cd12a7
nazar <nhavronskyi@gmail.com>
2023-02-23 14:27:45 +0100

    created restSite project

---
+++ restSite/src/main/java/com/example/restSite/RestSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.restSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSiteApplication.class, args);
+	}
+
+}
---
+++ restSite/src/test/java/com/example/restSite/RestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
