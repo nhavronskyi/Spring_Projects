fd9e7e0e0e91831a8eb1b844011db0724fcad344
nazar <nhavronskyi@gmail.com>
2023-02-23 10:32:13 +0100

    create index.html and modify pom.xml

--- mySite/src/main/java/com/example/mySite/clients/ClientController.java
+++ mySite/src/main/java/com/example/mySite/clients/ClientController.java
@@ -3,21 +3,20 @@ package com.example.mySite.clients;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ClientController {
 	
 	private final ClientService clientService;
 	
-	@RequestMapping("/hello")
-	public String showHello() {
-		return "Hello client";
+	@GetMapping("/hello")
+	public String hello() {
+		return "index";
 	}
 	
 	@Autowired
