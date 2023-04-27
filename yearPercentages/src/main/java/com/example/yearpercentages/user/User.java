923a0585cd560a57ff945a4ccb5eebc700e7283d
nazar <nhavronskyi@gmail.com>
2023-04-27 19:53:32 +0200

    smth wrong with all...

--- yearPercentages/src/main/java/com/example/yearpercentages/user/User.java
+++ yearPercentages/src/main/java/com/example/yearpercentages/user/User.java
@@ -1,9 +1,9 @@
 package com.example.yearpercentages.user;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,10 +11,10 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Entity
-@Table(name = "user")
+//@Entity
+//@Table(name = "user")
 public class User{
-    @Id
+    //@Id
     private long id;
     private String userName;
     private boolean isStarted;
