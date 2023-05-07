e0e3315fbd04f2242c598e52259f9ff86a32189d
nhavronskyi <96009885+nhavronskyi@users.noreply.github.com>
2023-05-07 11:02:21 +0200

    Delete mySite directory

--- mySite/src/main/java/com/example/mySite/clients/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mySite.clients;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
