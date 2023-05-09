0395a8e6f91ea5a2e5d031b9c7a80bddf4931137
nazar <nhavronskyi@gmail.com>
2023-05-09 07:20:17 +0200

    messed code

---
+++ googleSheetsDemo/src/test/java/com/example/googlesheetsdemo/GoogleSheetsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlesheetsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSheetsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
