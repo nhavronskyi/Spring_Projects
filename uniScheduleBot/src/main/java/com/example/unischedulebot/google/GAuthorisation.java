4b79d8fd206710e528a3fe54b8993687a9959975
nazar <nhavronskyi@gmail.com>
2023-05-09 18:03:49 +0200

    all works now

--- uniScheduleBot/src/main/java/com/example/unischedulebot/google/GAuthorisation.java
+++ uniScheduleBot/src/main/java/com/example/unischedulebot/google/GAuthorisation.java
@@ -13,33 +13,17 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.auth.oauth2.GoogleCredentials;
 import lombok.Getter;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 public class GAuthorisation {
     private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
     public Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT, List<String> SCOPES) throws IOException {
-        InputStream in = GAuthorisation.class.getResourceAsStream("/credentials.json");
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + "/credentials.json");
-        }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("token")))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("gSheetsAndCalendar");
-        //returns an authorized Credential object.
-        return credential;
+        var is = GAuthorisation.class.getResourceAsStream("/credentials.json");
+        return GoogleCredential.fromStream(Objects.requireNonNull(is))
+                .createScoped(SCOPES);
     }
 }
