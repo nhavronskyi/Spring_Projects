44ba56af7afdc2e45de91c0cbd1ba8f9cd8a78c7
nazar <nhavronskyi@gmail.com>
2023-05-23 09:44:40 +0200

    done

---
+++ toDoList/todo_list_frontend/src/TaskForm.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+export function TaskForm() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleChanges = (e: React.ChangeEvent<HTMLInputElement>): void =>
+        e.target.name === "title" ? setTitle(e.target.value) : setDescription(e.target.value);
+
+    const addTask = (e: React.FormEvent): void => {
+        const task = {title, description};
+
+        fetch("http://localhost:8080/notes", {
+            method: "post",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(task)
+        })
+    };
+
+    return (
+        <div>
+            <form onSubmit={addTask}>
+                <div>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        name="title"
+                        onChange={handleChanges}/>
+                </div>
+                <div>
+                    <input
+                        placeholder="Description"
+                        value={description}
+                        name="description"
+                        onChange={handleChanges}/>
+                </div>
+                <button type="submit">
+                    Add Task
+                </button>
+            </form>
+        </div>
+    );
+}
